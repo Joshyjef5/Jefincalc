5ee6ab1306adf17e74c87e04c0a43ce21f344c4c
Joshyjef5 <162860588+Joshyjef5@users.noreply.github.com>
2024-03-28 13:10:31 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,69 +1,42 @@
-history = []
 def add(x, y):
-    result = x + y
-    print("Result : {0} + {1} = {2}".format(x, y, result))
-    history.append("{0} + {1} = {2}".format(x, y, result))
+    return x + y
 
+def subtract(x, y):
+    return x - y
 
-def sub(x, y):
-    result = x - y
-    print("Result : {0} - {1} = {2}".format(x, y, result))
-    history.append("{0} + {1} = {2}".format(x, y, result))
+def multiply(x, y):
+    return x * y
 
-
-def mul(x, y):
-    result = x * y
-    print("Result : {0} * {1} = {2}".format(x, y, result))
-    history.append("{0} * {1} = {2}".format(x, y, result))
-
-
-def div(x, y):
+def divide(x, y):
     if y == 0:
-        print("Cannot divide by zero")
-
+        return "Error! Division by zero."
     else:
-        result = x / y
-        print("Result : {0} / {1} = {2}".format(x, y, result))
-        history.append("{0} / {1} = {2}".format(x, y, result))
+        return x / y
 
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
 
-def display_history():
-    for i in history:
-        print(i)
+while True:
+    choice = input("Enter choice (1/2/3/4): ")
 
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-while 1:
-    print("Calculator")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Show History")
-    print("6. Exit")
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
 
-    n = int(input("Enter your choice (1-6): "))
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
 
-    if n == 6:
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
         break
-
-    elif n == 5:
-        display_history()
-
-    elif n > 6:
-        print("Invalid choice.Please enter a number between 1 and 6.")
-
     else:
-        a = float(input("Enter first number: "))
-        b = float(input("Enter second number: "))
-
-        if n == 1:
-            add(a, b)
-
-        elif n == 2:
-            sub(a, b)
-
-        elif n == 3:
-            mul(a, b)
-
-        elif n == 4:
-            div(a, b)
+        print("Invalid Input")
